f441d5400c7a686bc88ec8b7bbf3d7a4810042d7
Hyunseok Shim <shim1210@gmail.com>
2026-07-26 21:01:24 +0900

    소스 자동 업로드

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
           <span className="logo-mark">N</span>
           <span>NexDataForge</span> 
         </a>
-        이 홈페이지는 현재 개발중입니다. 일부 기능은 정상적으로 작동하지 안될수 있습니다. (Beta)
+        이 홈페이지는 현재 개발중입니다. 상업용이 아닌 개인 개발/테스트 홈페이지입니다. (Beta)
         <nav className="main-nav" aria-label="주요 메뉴">
           <a href="#about">소개</a>
           <a href="#services">서비스</a>
